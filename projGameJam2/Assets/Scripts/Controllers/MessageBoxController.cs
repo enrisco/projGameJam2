3b18a2ef93205a813711c6ec6cf4122c9f6f95ec
enrisco <64703041+enrisco@users.noreply.github.com>
2023-04-23 20:33:30 -0300

    ehudhygdyugguygudh\iygyigd

--- projGameJam2/Assets/Scripts/Controllers/MessageBoxController.cs
+++ projGameJam2/Assets/Scripts/Controllers/MessageBoxController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEditor.VersionControl;
 using UnityEngine;
 
 class MessageBoxController : MonoBehaviour
