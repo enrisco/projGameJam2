6f14704f1ae2356eb987d0c8ac16985a8762447f
enrisco <64703041+enrisco@users.noreply.github.com>
2023-04-23 15:24:29 -0300

    v0.0.7
    -correção de bug

--- projGameJam2/Assets/Scripts/Controllers/Interactables/TestController.cs
+++ projGameJam2/Assets/Scripts/Controllers/Interactables/TestController.cs
@@ -9,9 +9,11 @@ class TestController : MonoBehaviour
 {
     [SerializeField] PlayerController Player;
     [SerializeField] MessageBoxController MessageBoxController;
+
+    [SerializeField] float Distance;
     private void Update()
     {
-        if (Vector2.Distance(transform.position, Player.transform.position) < 1 && Input.GetKeyDown(KeyCode.Z) && Player.CanMove)
+        if (Vector2.Distance(transform.position, Player.transform.position) < Distance && Input.GetKeyDown(KeyCode.Z) && Player.CanMove)
         {
             MessageBoxController.SetMessages("Nicole", new string[1] { "Oi, eu sou a Nicole e sou muito triste" }, -1, true, true);
             Player.CanMove = false;
